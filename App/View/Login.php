685a0f93d989fdbca04c2dcba5f62900f335f3e3
gefferey <jhzhang@astreetpress.com>
2013-04-19 12:48:07 +0800

    dd

--- App/View/Login.php
+++ App/View/Login.php
@@ -3,6 +3,19 @@ class View_Login extends View_Base {
 	
 	public function renderMain() {
 		
-		return 'please login';
+		$return =<<<html
+		
+		<div class="auth-form-body">
+			<label for="login_field"> Username or Email </label>
+			<input id="login_field" class="input-block" type="text" tabindex="1" name="login" autofocus="autofocus" autocapitalize="off">
+			<label for="password">
+				Password
+			</label>
+			<input id="password" class="input-block" type="password" tabindex="2" name="password" autocomplete="disabled">
+			<input class="button" type="submit" value="Sign in" tabindex="3" name="commit">
+		</div>
+html;
+	
+		return $return;
 	}
 }

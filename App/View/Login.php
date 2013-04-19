60d121a95e1a801a748a7947740b16b3c80c42e2
gefferey <jhzhang@astreetpress.com>
2013-04-19 13:15:11 +0800

    dd

--- App/View/Login.php
+++ App/View/Login.php
@@ -8,7 +8,7 @@ class View_Login extends View_Base {
 			<h1>Sign in</h1>
 		</div>
 		<div class="auth-form-body">
-			<label for="login_field"> Username or Email </label>
+			<label for="login_field"> Username </label>
 			<input id="login_field" class="input-block" type="text" tabindex="1" name="login" autofocus="autofocus" autocapitalize="off">
 			<label for="password">
 				Password

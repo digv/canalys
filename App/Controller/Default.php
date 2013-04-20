fcae54f74c374ad6e21ca46ec6653994d6184e4e
gefferey <250276522@qq.com>
2013-04-20 16:29:49 +0800

    dd

--- App/Controller/Default.php
+++ App/Controller/Default.php
@@ -5,14 +5,14 @@ class Controller_Default extends Core_Controller {
 	public function handleDefault() {
 		$this->mustLogin();
 		$model = new Database_WorkAssignment();
-		$view = $this->prepView('View_Staff', $model);
+		$view = $this->prepView('View_WorkAssignment', $model);
 		$view -> render ();
 	}
 	
 	public function handleQbf() {
 		$this->mustLogin ();
 		$model = new Database_WorkAssignment ();
-		$view = $this->prepView ( 'View_Staff', $model );
+		$view = $this->prepView ( 'View_WorkAssignment', $model );
 		$view->renderAjax ();
 	}
 }
